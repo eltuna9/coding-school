88da59c02bbfa9bc2b18e5c09cde389484a28a7f
Nicol Mouton <nicolmouton@yahoo.com>
2021-04-25 16:15:48 +1000

    Began building to do list with html, css, js. Js not working

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 console.log("first hello world application running correctly bItcHssssss");
+function myfunctionsubmit() {
+  var li = document.createElement("li");
+}
+var inputValue = document.getElementById("mytext").value;
+var t = document.createTextNode(inputValue);
+li.appendChild(t);
